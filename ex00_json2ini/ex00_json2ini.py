99c78f9d62d3d9d77a398ca3f1309f8d4e5a87c1
lotusbleudesign <lotus.b78@gmail.com>
2021-09-23 11:55:33 +0200

    exo 04 Hangman

--- ex00_json2ini/ex00_json2ini.py
+++ ex00_json2ini/ex00_json2ini.py
@@ -8,16 +8,16 @@ if os.path.exists(file):
     with open(file,"r") as file : 
         data = json.load(file)
         
-    data.keys()
-    
     for section in data.keys():
         string += "[" + section + "]" + "\n"
-        for values in data[section].keys():
-            string +=  values + " = " + data[section][values] + "\n"
+        sectionElement = data[section]
+        for key in sectionElement.keys():
+            string +=  key + " = " + sectionElement[key] + "\n"
         string += "\n"
         
     with open('file.ini', 'w') as file:
         file.write(string)
+        print("Job done !")
        
 else:
     print("File doesn't exist .. ")
